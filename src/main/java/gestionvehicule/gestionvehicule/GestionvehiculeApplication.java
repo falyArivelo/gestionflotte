29570ec8a9756629af8d1a6417726649a31b54bb
arivelofaly <119029443+falyArivelo@users.noreply.github.com>
2023-11-11 16:53:26 +0300

    first commit

---
+++ src/main/java/gestionvehicule/gestionvehicule/GestionvehiculeApplication.java
@@ -0,0 +1,13 @@
+package gestionvehicule.gestionvehicule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionvehiculeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionvehiculeApplication.class, args);
+	}
+
+}
